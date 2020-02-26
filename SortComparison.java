2a79b10627496e2f3ccc314f799bfeb9a0e52940
Brian Farrell <55883565+branflakes6@users.noreply.github.com>
2020-02-26 15:08:39 +0000

    Added code to run experiments and added the results of these to the top of the file

--- SortComparison.java
+++ SortComparison.java
@@ -1,4 +1,55 @@
 // -------------------------------------------------------------------------
+/**
+ It took 0.0108ms to run numbers10.txt using selectionSort
+ It took 0.2259ms to run numbers100.txt using selectionSort
+ It took 14.2167ms to run numbers1000.txt using selectionSort
+ It took 2.4777ms to run numbers1000Duplicates.txt using selectionSort
+ It took 2.3665ms to run numbersNearlyOrdered1000.txt using selectionSort
+ It took 3.9727ms to run numbersReverse1000.txt using selectionSort
+ It took 9.203ms to run numbersSorted1000.txt using selectionSort
+
+
+ It took 0.0129ms to run numbers10.txt using insertionSort
+ It took 0.1356ms to run numbers100.txt using insertionSort
+ It took 6.7589ms to run numbers1000.txt using insertionSort
+ It took 2.344ms to run numbers1000Duplicates.txt using insertionSort
+ It took 0.4139ms to run numbersNearlyOrdered1000.txt using insertionSort
+ It took 2.9756ms to run numbersReverse1000.txt using insertionSort
+ It took 0.0083ms to run numbersSorted1000.txt using insertionSort
+
+
+ It took 0.0902ms to run numbers10.txt using mergeSortIterative
+ It took 0.0938ms to run numbers100.txt using mergeSortIterative
+ It took 0.7587ms to run numbers1000.txt using mergeSortIterative
+ It took 0.6657ms to run numbers1000Duplicates.txt using mergeSortIterative
+ It took 2.6084ms to run numbersNearlyOrdered1000.txt using mergeSortIterative
+ It took 0.2925ms to run numbersReverse1000.txt using mergeSortIterative
+ It took 0.2562ms to run numbersSorted1000.txt using mergeSortIterative
+
+
+ It took 0.0285ms to run numbers10.txt using mergeSortRecursive
+ It took 0.1321ms to run numbers100.txt using mergeSortRecursive
+ It took 1.2591ms to run numbers1000.txt using mergeSortRecursive
+ It took 0.4936ms to run numbers1000Duplicates.txt using mergeSortRecursive
+ It took 0.353ms to run numbersNearlyOrdered1000.txt using mergeSortRecursive
+ It took 0.3211ms to run numbersReverse1000.txt using mergeSortRecursive
+ It took 0.3439ms to run numbersSorted1000.txt using mergeSortRecursive
+
+
+ It took 0.0213ms to run numbers10.txt using quickSort
+ It took 0.0483ms to run numbers100.txt using quickSort
+ It took 1.2804ms to run numbers1000.txt using quickSort
+ It took 0.2371ms to run numbers1000Duplicates.txt using quickSort
+ It took 0.8098ms to run numbersNearlyOrdered1000.txt using quickSort
+ It took 4.6526ms to run numbersReverse1000.txt using quickSort
+ It took 1.6836ms to run numbersSorted1000.txt using quickSort
+
+ */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *  This class contains static methods that implementing sorting of an array of numbers
@@ -19,17 +70,19 @@ class SortComparison {
      */
     static double [] insertionSort (double a[])
     {
-        for (int i = 1; i < a.length; ++i) {
-            double temp = a[i];
-            int j = i - 1;
-            while (j >= 0 && a[j] > temp)
-            {
-            a[j+1] = a[j];
-            j = j - 1;
+        if(a != null) {
+            for (int i = 1; i < a.length; ++i) {
+                double temp = a[i];
+                int j = i - 1;
+                while (j >= 0 && a[j] > temp) {
+                    a[j + 1] = a[j];
+                    j = j - 1;
+                }
+                a[j + 1] = temp;
             }
-            a[j+1] = temp;
+            return a;
         }
-        return a;
+        return null;
     }
 
     /**
@@ -41,21 +94,21 @@ class SortComparison {
      */
     static double [] selectionSort (double a[])
     {
-        for (int i = 0; i < a.length-1; i++)
-        {
-            int index = i;
-            for (int j = i+1; j < a.length; j++)
-            {
-                if (a[j] < a[index])
-                {
-                index = j;
+        if(a != null) {
+            for (int i = 0; i < a.length - 1; i++) {
+                int index = i;
+                for (int j = i + 1; j < a.length; j++) {
+                    if (a[j] < a[index]) {
+                        index = j;
+                    }
                 }
+                double temp = a[index];
+                a[index] = a[i];
+                a[i] = temp;
             }
-            double temp = a[index];
-            a[index] = a[i];
-            a[i] = temp;
+            return a;
         }
-    return a;
+        return null;
     }
 
     /**
@@ -66,8 +119,11 @@ class SortComparison {
      *
      */
     static double [] quickSort (double a[]){
-        quickSort(a, 0, a.length - 1);
-        return a;
+        if(a!=null) {
+            quickSort(a, 0, a.length - 1);
+            return a;
+        }
+        return null;
     }
 
     private static void quickSort(double a[], int lo, int hi)
@@ -162,8 +218,11 @@ class SortComparison {
      */
     static double[] mergeSortRecursive (double a[])
     {
-        mergeSortRecursive(a, 0, a.length-1);
-        return a;
+        if(a != null) {
+            mergeSortRecursive(a, 0, a.length - 1);
+            return a;
+        }
+        return null;
     }
     private  static void mergeSortRecursive (double[] a,  int lo, int hi)
     {
@@ -215,9 +274,127 @@ class SortComparison {
             k++;
         }
     }
-
     public static void main(String[] args) {
+        String[] tests = new String[]{"C:/Users/brian/Downloads/assignment input data files/numbers10.txt",
+                "C:/Users/brian/Downloads/assignment input data files/numbers100.txt",
+                "C:/Users/brian/Downloads/assignment input data files/numbers1000.txt",
+                "C:/Users/brian/Downloads/assignment input data files/numbers1000Duplicates.txt",
+                "C:/Users/brian/Downloads/assignment input data files/numbersNearlyOrdered1000.txt",
+                "C:/Users/brian/Downloads/assignment input data files/numbersReverse1000.txt",
+                "C:/Users/brian/Downloads/assignment input data files/numbersSorted1000.txt"};
+            for (String file : tests) {
+            ArrayList<Double> numbers = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) numbers.add(Double.parseDouble(line));
+                }
+                double[] a = new double[numbers.size()];
+                for (int j = 0; j < a.length; j++) {
+                    a[j] = numbers.get(j);
+                }
+                double start = System.nanoTime();
+                selectionSort(a);
+                double end = System.nanoTime();
+                double total = (end - start) / 1000000;
+                System.out.println("It took " + total + "ms" + " to run " + file.split("/")[file.split("/").length - 1] + " using selectionSort");
 
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+            System.out.println("\n");
+        for (String file : tests) {
+            ArrayList<Double> numbers = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) numbers.add(Double.parseDouble(line));
+                }
+                double[] a = new double[numbers.size()];
+                for (int j = 0; j < a.length; j++) {
+                    a[j] = numbers.get(j);
+                }
+                double start = System.nanoTime();
+                insertionSort(a);
+                double end = System.nanoTime();
+                double total = (end - start) / 1000000;
+                System.out.println("It took " + total + "ms" + " to run " + file.split("/")[file.split("/").length - 1] + " using insertionSort");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n");
+        for (String file : tests) {
+            ArrayList<Double> numbers = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) numbers.add(Double.parseDouble(line));
+                }
+                double[] a = new double[numbers.size()];
+                for (int j = 0; j < a.length; j++) {
+                    a[j] = numbers.get(j);
+                }
+                double start = System.nanoTime();
+                mergeSortIterative(a);
+                double end = System.nanoTime();
+                double total = (end - start) / 1000000;
+                System.out.println("It took " + total + "ms" + " to run " + file.split("/")[file.split("/").length - 1] + " using mergeSortIterative");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n");
+        for (String file : tests) {
+            ArrayList<Double> numbers = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) numbers.add(Double.parseDouble(line));
+                }
+                double[] a = new double[numbers.size()];
+                for (int j = 0; j < a.length; j++) {
+                    a[j] = numbers.get(j);
+                }
+                double start = System.nanoTime();
+                mergeSortRecursive(a);
+                double end = System.nanoTime();
+                double total = (end - start) / 1000000;
+                System.out.println("It took " + total + "ms" + " to run " + file.split("/")[file.split("/").length - 1] + " using mergeSortRecursive");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n");
+        for (String file : tests) {
+            ArrayList<Double> numbers = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) numbers.add(Double.parseDouble(line));
+                }
+                double[] a = new double[numbers.size()];
+                for (int j = 0; j < a.length; j++) {
+                    a[j] = numbers.get(j);
+                }
+                double start = System.nanoTime();
+                quickSort(a);
+                double end = System.nanoTime();
+                double total = (end - start) / 1000000;
+                System.out.println("It took " + total + "ms" + " to run " + file.split("/")[file.split("/").length - 1] + " using quickSort");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
-
 }
--- sortComparisonTest.java
+++ sortComparisonTest.java
@@ -1,9 +1,15 @@
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 //-------------------------------------------------------------------------
 /**
@@ -13,49 +19,175 @@ import org.junit.runners.JUnit4;
  *  @version HT 2020
  */
 @RunWith(JUnit4.class)
-public class sortComparisonTest
-{
+public class sortComparisonTest {
 
     //~ Constructor ........................................................
     @Test
-    public void testConstructor()
-    {
+    public void testConstructor() {
         new SortComparison();
     }
 
     @Test
-    public void testInsertionSort()
-    {
-        double[] a = new double[] {3.2,5.1,1.56,2.34,8.9,6.12,4.2} ;
-        double[] b = new double[] {1.56,2.34,3.2,4.2,5.1,6.12,8.9} ;
-        assertArrayEquals(SortComparison.insertionSort(a),b, 0);
+    public void testInsertionSort() {
+        double[] a = new double[]{3.2, 5.1, 1.56, 2.34, 8.9, 6.12, 4.2};
+        double[] b = new double[]{1.56, 2.34, 3.2, 4.2, 5.1, 6.12, 8.9};
+        assertArrayEquals(SortComparison.insertionSort(a), b, 0);
+        assertNull(SortComparison.insertionSort(null));
     }
+
     @Test
-    public void testSelectionSort()
-    {
-        double[] a = new double[] {3.2,5.1,1.56,2.34,8.9,6.12,4.2} ;
-        double[] b = new double[] {1.56,2.34,3.2,4.2,5.1,6.12,8.9} ;
-        assertArrayEquals(SortComparison.selectionSort(a),b, 0);
+    public void testSelectionSort() {
+        double[] a = new double[]{3.2, 5.1, 1.56, 2.34, 8.9, 6.12, 4.2};
+        double[] b = new double[]{1.56, 2.34, 3.2, 4.2, 5.1, 6.12, 8.9};
+        assertArrayEquals(SortComparison.selectionSort(a), b, 0);
+        assertNull(SortComparison.insertionSort(null));
     }
+
     @Test
-    public void testQuickSort()
-    {
-        double[] a = new double[] {3.2,5.1,1.56,2.34,8.9,6.12,4.2} ;
-        double[] b = new double[] {1.56,2.34,3.2,4.2,5.1,6.12,8.9} ;
-        assertArrayEquals(SortComparison.quickSort(a),b, 0);
+    public void testQuickSort() {
+        double[] a = new double[]{3.2, 5.1, 1.56, 2.34, 8.9, 6.12, 4.2};
+        double[] b = new double[]{1.56, 2.34, 3.2, 4.2, 5.1, 6.12, 8.9};
+        assertArrayEquals(SortComparison.quickSort(a), b, 0);
+        assertNull(SortComparison.insertionSort(null));
     }
+
     @Test
-    public void testMergeSortIterative()
-    {
-        double[] a = new double[] {3.2,5.1,1.56,2.34,8.9,6.12,4.2} ;
-        double[] b = new double[] {1.56,2.34,3.2,4.2,5.1,6.12,8.9} ;
-        assertArrayEquals(SortComparison.mergeSortIterative(a),b, 0);
+    public void testMergeSortIterative() {
+        double[] a = new double[]{3.2, 5.1, 1.56, 2.34, 8.9, 6.12, 4.2};
+        double[] b = new double[]{1.56, 2.34, 3.2, 4.2, 5.1, 6.12, 8.9};
+        assertArrayEquals(SortComparison.mergeSortIterative(a), b, 0);
+        assertNull(SortComparison.insertionSort(null));
     }
+
     @Test
-    public void testMergeSortRecursive()
-    {
-        double[] a = new double[] {3.2,5.1,1.56,2.34,8.9,6.12,4.2} ;
-        double[] b = new double[] {1.56,2.34,3.2,4.2,5.1,6.12,8.9} ;
-        assertArrayEquals(SortComparison.mergeSortRecursive(a),b, 0);
+    public void testMergeSortRecursive() {
+        double[] a = new double[]{3.2, 5.1, 1.56, 2.34, 8.9, 6.12, 4.2};
+        double[] b = new double[]{1.56, 2.34, 3.2, 4.2, 5.1, 6.12, 8.9};
+        assertArrayEquals(SortComparison.mergeSortRecursive(a), b, 0);
+        assertNull(SortComparison.insertionSort(null));
     }
+    public static void main(String[] args)
+    {String[] tests = new String[]{"C:/Users/brian/Downloads/assignment input data files/numbers10.txt",
+            "C:/Users/brian/Downloads/assignment input data files/numbers100.txt",
+            "C:/Users/brian/Downloads/assignment input data files/numbers1000.txt",
+            "C:/Users/brian/Downloads/assignment input data files/numbers1000Duplicates.txt",
+            "C:/Users/brian/Downloads/assignment input data files/numbersNearlyOrdered1000.txt",
+            "C:/Users/brian/Downloads/assignment input data files/numbersReverse1000.txt",
+            "C:/Users/brian/Downloads/assignment input data files/numbersSorted1000.txt"};
+        for (String file : tests) {
+            ArrayList<Double> numbers = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) numbers.add(Double.parseDouble(line));
+                }
+                double[] a = new double[numbers.size()];
+                for (int j = 0; j < a.length; j++) {
+                    a[j] = numbers.get(j);
+                }
+                double start = System.nanoTime();
+                SortComparison.selectionSort(a);
+                double end = System.nanoTime();
+                double total = (end - start) / 1000000;
+                System.out.println("It took " + total + "ms" + " to run " + file.split("/")[file.split("/").length - 1] + " using selectionSort");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n");
+        for (String file : tests) {
+            ArrayList<Double> numbers = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) numbers.add(Double.parseDouble(line));
+                }
+                double[] a = new double[numbers.size()];
+                for (int j = 0; j < a.length; j++) {
+                    a[j] = numbers.get(j);
+                }
+                double start = System.nanoTime();
+                SortComparison.insertionSort(a);
+                double end = System.nanoTime();
+                double total = (end - start) / 1000000;
+                System.out.println("It took " + total + "ms" + " to run " + file.split("/")[file.split("/").length - 1] + " using insertionSort");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n");
+        for (String file : tests) {
+            ArrayList<Double> numbers = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) numbers.add(Double.parseDouble(line));
+                }
+                double[] a = new double[numbers.size()];
+                for (int j = 0; j < a.length; j++) {
+                    a[j] = numbers.get(j);
+                }
+                double start = System.nanoTime();
+                SortComparison.mergeSortIterative(a);
+                double end = System.nanoTime();
+                double total = (end - start) / 1000000;
+                System.out.println("It took " + total + "ms" + " to run " + file.split("/")[file.split("/").length - 1] + " using mergeSortIterative");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n");
+        for (String file : tests) {
+            ArrayList<Double> numbers = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) numbers.add(Double.parseDouble(line));
+                }
+                double[] a = new double[numbers.size()];
+                for (int j = 0; j < a.length; j++) {
+                    a[j] = numbers.get(j);
+                }
+                double start = System.nanoTime();
+                SortComparison.mergeSortRecursive(a);
+                double end = System.nanoTime();
+                double total = (end - start) / 1000000;
+                System.out.println("It took " + total + "ms" + " to run " + file.split("/")[file.split("/").length - 1] + " using mergeSortRecursive");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("\n");
+        for (String file : tests) {
+            ArrayList<Double> numbers = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+                String line = br.readLine();
+                while (line != null) {
+                    line = br.readLine();
+                    if (line != null) numbers.add(Double.parseDouble(line));
+                }
+                double[] a = new double[numbers.size()];
+                for (int j = 0; j < a.length; j++) {
+                    a[j] = numbers.get(j);
+                }
+                double start = System.nanoTime();
+                SortComparison.quickSort(a);
+                double end = System.nanoTime();
+                double total = (end - start) / 1000000;
+                System.out.println("It took " + total + "ms" + " to run " + file.split("/")[file.split("/").length - 1] + " using quickSort");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
